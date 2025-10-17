403261631e9e1e1cd840fb73c5e4ff7757a8e5b2
BharatYadav05 <144779835+BharatYadav05@users.noreply.github.com>
2025-10-17 13:40:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function addTask() {
+    const inputBox = document.querySelector('.input-area input[type="text"]');
+    const taskText = inputBox.value.trim();
+    const taskList = document.getElementById('addTask');
+
+    if (taskText === '') {
+        alert("Please enter a Task !");
+        return;
+    }
+
+    const li = document.createElement('li');
+
+    li.textContent = taskText;
+
+    taskList.appendChild(li);
+
+    inputBox.value = '';
+}
